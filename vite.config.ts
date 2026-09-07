43c04e07e0f46519a16ddd7892022f7c0492942f
BlankS0800 <perezovandohernan@gmail.com>
2026-09-06 22:42:42 -0400

    fix: vite config 2

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/TUnder/', // <- Reemplaza con el nombre exacto de tu repo en GitHub
+  base: '/TUnder/'
 })
